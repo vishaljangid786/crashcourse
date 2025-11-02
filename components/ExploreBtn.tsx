666a860f2e5506305799e598dcc57e65d61d8a85
vishal <vishaljangid80550786@gmail.com>
2025-11-02 10:55:40 +0530

    demo done

---
+++ components/ExploreBtn.tsx
@@ -0,0 +1,12 @@
+'use client'
+import Image from "next/image";
+
+const ExploreBtn = () => {
+    return (
+        <button type='button' id='explore-btn' className='mt-7 mx-auto' onClick={()=>console.log('click')}>
+            <a href='#events'>Explore Events</a>
+            <Image src='/icons/arrow-down.svg' alt='arrow-down'  width={24} height={24} />
+        </button>
+    )
+}
+export default ExploreBtn
