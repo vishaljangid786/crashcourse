05d47fdddd847f1e0d5bcd4bb63e2015c53a13be
vishal <vishaljangid80550786@gmail.com>
2025-11-02 10:56:42 +0530

    commit

---
+++ lib/constants.ts
@@ -0,0 +1,75 @@
+export const events: {
+  title: string;
+  image: string;
+  slug: string;
+  location: string;
+  date: string; // ISO or human-readable date string
+  time: string; // Local time string
+}[] = [
+  {
+    title: "React Summit Europe 2025",
+    image: "/images/event2.png",
+    slug: "react-summit-europe-2025",
+    location: "Amsterdam, NL",
+    date: "2025-11-18",
+    time: "09:00 AM",
+  },
+  {
+    title: "Next.js Global Conference",
+    image: "/images/event3.png",
+    slug: "nextjs-global-2025",
+    location: "San Francisco, USA",
+    date: "2025-12-05",
+    time: "10:00 AM",
+  },
+  {
+    title: "Open Source Festival",
+    image: "/images/event6.png",
+    slug: "open-source-fest-2026",
+    location: "Berlin, DE",
+    date: "2026-01-22",
+    time: "11:00 AM",
+  },
+  {
+    title: "AI & Web Dev Days",
+    image: "/images/event-full.png",
+    slug: "ai-web-dev-days-2026",
+    location: "London, UK",
+    date: "2026-02-12",
+    time: "09:30 AM",
+  },
+  {
+    title: "Next.js Global Conference",
+    image: "/images/event3.png",
+    slug: "nextjs-glloud Native Conf",
+    image: "/images/event2.png",
+    slug: "cloud-native-conf-2026",
+    location: "Bangalore, IN",
+    date: "2026-03-03",
+    time: "09:00 AM",
+  },
+  {
+    title: "JavaScript Nation Summit",
+    image: "/images/event3.png",
+    slug: "javascript-nation-2026",
+    location: "Toronto, CA",
+    date: "2026-03-19",
+    time: "10:00 AM",
+  },
+  {
+    title: "DevOps & SRE World",
+    image: "/images/event6.png",
+    slug: "devops-sre-world-2026",
+    location: "Sydney, AU",
+    date: "2026-04-08",
+    time: "09:00 AM",
+  },
+  {
+    title: "Global Hack Week: Web Edition",
+    image: "/images/event-full.png",
+    slug: "global-hack-week-web-2026",
+    location: "Remote / Online",
+    date: "2026-04-25",
+    time: "All Day",
+  },
+];
